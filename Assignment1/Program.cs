9d59192efd9d25ca2fe80d48bd38a8e461a9637f
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 20:09:47 +0530

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringConcatenationLibrary;
+
+namespace Assignment1
+{
+    public  class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            A a = new A();
+            AttachStringToEachother attachDelagate =a.ConcatStrings;
+            string result = attachDelagate("hello", "world");
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ StringConcatenationLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringConcatenationLibrary
+{
+    public delegate string AttachStringToEachother(string s1 , string s2 ); 
+    public class A
+    {
+        public string ConcatStrings(string s1,string s2)
+        {
+            return s1 + s2;
+        }
+
+    }
+}
